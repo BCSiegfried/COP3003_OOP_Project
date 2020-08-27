f320786bd0cf91dbb5a22b1477e74a1859feb269
Brandon Siegfried <Bcsiegfried@eagle.fgcu.edu>
2020-08-27 15:25:22 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Main extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    Scene scene = new Scene(root, 600, 550);
+    primaryStage.setTitle("Production Project");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+    scene.getStylesheets().add("GUIStyle1.css");
+
+  }
+
+}
